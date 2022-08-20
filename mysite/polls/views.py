b094a95228f8e165cd5a0bb9c14bcdfab44e330d
Tri Phung <cadbane1998@gmail.com>
2022-08-20 15:54:33 +0000

    refactor: update url for detail view; write vote , resutl, detail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,14 +14,32 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 
-def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+def vote(request, question_id):
+    question = get_object_or_404 (Question, question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,
+                      'polls/detail.html',
+                      {
+                        'question': question,
+                        'error_message': "You didn't select a choice.",
+                      }
+                    )
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(
+                  reverse('polls:results', args=(question.id,) )
+                )
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
-def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def owner(request):
     return HttpResponse("Hello, world. 223c4a00 is the polls index")
