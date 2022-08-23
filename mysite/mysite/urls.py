b3e75485d7f1e863f89ffcd8e5fb706710d35886
Tri Phung <cadbane1998@gmail.com>
2022-08-23 19:32:02 +0000

    feat(home): add app for home, reroute url, configure settings at INSTALLED_APPS

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.views.static import serve
+from django.views.generic import TemplateView
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,4 +31,5 @@ urlpatterns = [
         {'document_root': SITE_ROOT,'show_indexes': True},
         name='site_path'
     ),
+    path('', TemplateView.as_view(template_name='home/main.html')),
 ]
