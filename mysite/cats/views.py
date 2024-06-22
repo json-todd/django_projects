ee60acac4407105763a843a13fbe835a13ac65ea
json-todd <cadbane1998@gmail.com>
2024-06-22 23:27:36 +0300

    feat(apps): write more views

--- mysite/cats/views.py
+++ mysite/cats/views.py
@@ -17,19 +17,24 @@ class CatView(LoginRequiredMixin, View):
         ctx = {'breed_count': breed_count, 'cat_list': cat_list}
         return render(request, 'cats/cat_list.html', context=ctx)
     
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+    
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    fields = '__all__'
+    success_urls = reverse_lazy('cats:all')
+
 class BreedView(LoginRequiredMixin, View):
     def get(self, request):
         breed_list = Breed.objects.all().count()
         ctx = {'breed_list': breed_list}
         return render(request,'cats/breed_list.html', context=ctx)
     
-class CatCreate(LoginRequiredMixin, CreateView):
-    success_url = reverse_lazy('cats:all')
-    
-class CatUpdate(LoginRequiredMixin, UpdateView):
-    pass
-
-class CatDelete(LoginRequiredMixin, DeleteView):
-    model = Cat
-    fields = '__all__'
-    success_urls = reverse_lazy('cats:all')
